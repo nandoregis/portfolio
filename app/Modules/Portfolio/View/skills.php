1418273a59a4e7da17fb5794182dca240bb9fd6f
Fernando Regis <nando.1regis@gmail.com>
2026-03-03 21:28:22 -0300

    alterar codigo html, e criar um folha de estilo a parte.

--- app/Modules/Portfolio/View/skills.php
+++ app/Modules/Portfolio/View/skills.php
@@ -2,7 +2,23 @@
 
     <div class="my-skills">
 
-            <h2>skills</h2>
+            <div class="my-skills--model">
+                <div class="over"></div>
+            </div>
+            <div class="my-skills--cards">
+                <h2>Skills</h2>
+                <div class="my-skills--cards-group">
+                    <div class="my-skills--cards-item">
+                        <span>
+
+                        </span>
+                        <div class="my-skilss--cards-item__info">
+                            <h4>HTML</h4>
+                            <p>6 anos entre estudo e prática na programação.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
     </div>
 
