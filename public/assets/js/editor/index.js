51878a29e28268cdc8de36dca895b8140dbba786
Fernando Regis <nando.1regis@gmail.com>
2026-03-03 22:23:23 -0300

    modifica js para chamar modelo 3D

--- public/assets/js/editor/index.js
+++ public/assets/js/editor/index.js
@@ -7,24 +7,10 @@ const loading = document.querySelector('.tools__loading');
 
 ( async () => {
   
+    const renderModel = document.getElementById('render');
     const view = new Viewer(renderModel);
-    const urlApi = 'https://visualapi.nellure.com'; //
-
-    const uuidModel = document.getElementById('uuidModelo').value; //
-    const uuidReferencia = document.getElementById('uuidRef').value; //
-
-    const textures = await fetch(`${urlApi}/v1/references/textures/${uuidReferencia}`);
-    const texturesJson = await textures.json();
-    
-    const model  = await fetch(`${urlApi}/v1/models/${uuidModel}`);
-    const modelJson = await model.json();
-    
-    let modelo3d = await view.addModel(modelJson.diretorio); 
-    
-    await enableUVMolde(modelJson.diretorio_uv, modelo3d, texturesJson);
+    const modelo = await view.addModel('http://localhost/camiseta.glb'); 
     await view.run();
-    
-    loading.classList.add('hide');
 
 })();
 
