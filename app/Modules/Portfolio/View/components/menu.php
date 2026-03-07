6a65f0233d0a8314eb6e983bf4581c87c813f61b
Fernando Regis <nando.1regis@gmail.com>
2026-03-06 22:07:14 -0300

    animar menu de navegação

--- app/Modules/Portfolio/View/components/menu.php
+++ app/Modules/Portfolio/View/components/menu.php
@@ -11,25 +11,64 @@
     <script>
         const buttons = document.querySelectorAll('.menu nav a');
         
+        
+
+
+        const scrollSmooth = (target) => {
+            
+            const startScroll = window.scrollY;
+            const distance = target - startScroll;
+            const duration = 600;
+            let startTime = null
+
+
+            console.log('target : ',target);
+            console.log('distance : ',distance);
+            console.log('startScroll : ',startScroll);
+
+
+            function animation ( currentTime ) {
+
+                if(!startTime) startTime = currentTime;
+
+                const elapsedTime = currentTime - startTime;
+                const progress = Math.min(elapsedTime / duration, 1);
+
+                const ease = progress < 0.5 
+                ?  4 * progress * progress * progress 
+                :  (progress - 1) * (2 * progress - 2) * (2 * progress - 2) + 1
+
+                window.scrollTo(0, startScroll + distance * ease);
+
+                console.log(startScroll + distance * ease)
+
+                if(elapsedTime < duration ) {
+
+                    requestAnimationFrame(animation);
+                }
+
+            }
+
+            requestAnimationFrame(animation);
+        }
+
+
         buttons.forEach(btn => {
 
-            btn.addEventListener('click', (e) => {
+            btn.addEventListener('click', (e)=>{
+
                 e.preventDefault();
 
                 const id = btn.getAttribute('href');
                 const element = document.querySelector(id);
 
-                const offset = element.offsetTop;
-
-                window.scrollTo({
-                    top: offset,
-                    behavior: "smooth"
-                });
+                scrollSmooth(element.offsetTop);
 
             });
 
         });
-        
+
+       
 
 
     </script>
