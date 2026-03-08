5e39ea200627d9c38a9de79de10e5b73d4134112
Fernando Regis <nando.1regis@gmail.com>
2026-03-08 13:37:11 -0300

    add links de para contato

--- app/Modules/Portfolio/View/components/social.php
+++ app/Modules/Portfolio/View/components/social.php
@@ -1,6 +1,6 @@
 <div class="social">
     <ul>
-        <li><a href="#" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a></li>
-        <li><a href="#" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/lu%C3%ADs-fernando-regis-a7a473227/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a></li>
+        <li><a href="https://github.com/" target="_blank"><i class="fa-brands fa-github"></i></a></li>
     </ul>
 </div>
--- app/Modules/Portfolio/View/sobre.php
+++ app/Modules/Portfolio/View/sobre.php
@@ -13,9 +13,9 @@
             <h3>Pronto para fazer parte de seu <span>time</span></h3>
             <div class="my-contato--links">
                 <ul>
-                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-github"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                    <li><a href="https://wa.me/5581998213458?text=Ol%C3%A1%2C%20estou%20entrado%20em%20contato%20pelo%20link%20do%20seu%20portf%C3%B3lio." target="_blank"> <i class="fa-brands fa-whatsapp"></i> </a></li>
+                    <li><a href="https://github.com/" target="_blank"> <i class="fa-brands fa-github"></i> </a></li>
+                    <li><a href="https://www.linkedin.com/in/lu%C3%ADs-fernando-regis-a7a473227/" target="_blank" > <i class="fa-brands fa-linkedin-in"></i> </a></li>
                 </ul>
             </div>
         </div>
